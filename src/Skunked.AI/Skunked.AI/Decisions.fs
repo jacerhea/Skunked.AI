e59b6643e5fe94f8eebd82793d688d6d2a82bf86
Jace <jace.rhea@gmail.com>
2019-03-24 21:51:26 -0500

    Porting decisions from Skunked library.

--- src/Skunked.AI/Skunked.AI/Decisions.fs
+++ src/Skunked.AI/Skunked.AI/Decisions.fs
@@ -5,20 +5,36 @@ open Skunked.Score
 open Skunked.PlayingCards
 open System.Collections.Generic
 
-    // Indent all program elements within modules that are declared with an equal sign.
-    type AbstractAverageDecision(calculatorIn: ScoreCalculator) = 
-        let calculator = calculatorIn
+    let calculator = new ScoreCalculator();
+    let deck = new Deck()
+    let possibleRemaining cards = deck |> Seq.except cards
+    let combinations(cards: IEnumerable<Card>) = new Combinations<Card>(new List<Card>(cards), 4)
 
-        member this.BaseAverageDecision(hand: seq<Card>) = 
-            let handSet = new HashSet<Card>(hand)
-            let combinations = new Combinations<Card>(new List<Card>(hand), 4)
-            let deck = new Deck()
-            let possible = deck |> Seq.except handSet
-            this.GetPossibleCombos(combinations, possible)
-
-        member this.GetPossibleCombos(handCombinations: Combinations<Card>, 
-                            possibleStarterCards: Collections.seq<Card>) =
+    let getPossibleCombos(handCombinations: Combinations<Card>, possibleStarterCards: Collections.seq<Card>) =
                 handCombinations 
                 |> Seq.map(fun combo -> 
-                            let possibleScores = possibleStarterCards |> Seq.map(fun cutCard -> new ScoreWithCut(Cut = cutCard, Score = calculator.CountShowScore(cutCard, combo).Score)) |> Seq.toList
+                            let possibleScores = possibleStarterCards 
+                                                |> Seq.map(fun cutCard -> new ScoreWithCut(Cut = cutCard, Score = calculator.CountShowScore(cutCard, combo).Score)) 
                             new ComboPossibleScores(combo, new List<ScoreWithCut>(possibleScores)))
+
+    // Indent all program elements within modules that are declared with an equal sign.
+    let baseDecision(hand: seq<_>) = 
+        let handSet = new HashSet<Card>(hand)
+        getPossibleCombos(combinations(handSet), possibleRemaining(handSet))
+
+    let maxAverage cards = 
+        let highestCombo = baseDecision(cards) 
+                        |> Seq.maxBy (fun combo -> combo.GetScoreSummation())
+        cards |> Seq.except highestCombo.Combo
+
+    let minAverage cards = 
+        let lowestCombo = baseDecision(cards) 
+                        |> Seq.minBy (fun combo -> combo.GetScoreSummation())
+        cards |> Seq.except lowestCombo.Combo
+
+
+    let optimisticDecision(cards: IEnumerable<_>) = 
+        let handSet = new HashSet<Card>(cards)
+        let combinationResult = combinations handSet
+        let possible = possibleRemaining handSet
+        4
